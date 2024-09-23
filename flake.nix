37a4b18e3772d82891fbbf1a2c8e10ed0c5e6ecb
Benedikt von Blomberg <github@bvb.anonaddy.com>
2024-09-23 20:33:46 +0200

    allow unfree

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
     in {
     nixosConfigurations = {
       nixosX220 = lib.nixosSystem {
