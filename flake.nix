917119b2b9c74e9d42e7e14fe16674b81846246b
Benedikt von Blomberg <github@bvb.anonaddy.com>
2024-11-04 18:03:08 +0100

    switch to unstable

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "locker flockig";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixvim.url = "github:1randomguy/nixvim";
   };
