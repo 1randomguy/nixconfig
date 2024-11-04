10cc5877152159d3ada3fe78fd8e6d17265b89e7
Benedikt von Blomberg <github@bvb.anonaddy.com>
2024-11-04 17:58:39 +0100

    added autocleanup

--- nixos/common.nix
+++ nixos/common.nix
@@ -13,4 +13,10 @@
   users.defaultUserShell = pkgs.zsh;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 45d";
+  };
+  nix.optimise.automatic = true;
 }
