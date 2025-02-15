b96311166b288e605acbd77e79856b717431d93b
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-02-15 21:40:44 +0100

    japanese fonts

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -4,6 +4,8 @@
     packages = with pkgs; [
       jetbrains-mono 
       nerd-fonts.jetbrains-mono
+      noto-fonts-cjk-serif
+      noto-fonts-cjk-sans
     ];
     fontconfig = {
       defaultFonts = {
