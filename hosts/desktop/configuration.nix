b040f75a0cc1f30828218e088d13201c5ac7607f
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-03-14 23:55:06 +0100

    change to beta driver nvidia

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -23,11 +23,11 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true;
+    powerManagement.enable = false;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
   hardware.enableRedistributableFirmware = true;
   hardware.firmware = [pkgs.wireless-regdb];
