b353e13ddb37c6f8dccb01aa120fd4e9ea336900
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-04-24 21:30:06 +0200

    waydroid

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -3,4 +3,5 @@
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
   users.extraGroups.vboxusers.members = [ "bene" ];
+  virtualisation.waydroid.enable = true;
 }
