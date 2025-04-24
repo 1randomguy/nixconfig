4cf14d8ab446f13b096763f6b6a9f4e0253db4b1
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-04-24 21:40:06 +0200

    quick fix

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -2,6 +2,8 @@
 {
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
+  # temporary fix for https://github.com/NixOS/nixpkgs/issues/363887#issuecomment-2536693220https://github.com/NixOS/nixpkgs/issues/363887#issuecomment-2536693220https://github.com/NixOS/nixpkgs/issues/363887#issuecomment-2536693220
+  virtualisation.virtualbox.host.enableKvm = true;
   users.extraGroups.vboxusers.members = [ "bene" ];
   virtualisation.waydroid.enable = true;
 }
