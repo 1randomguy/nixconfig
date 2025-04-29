2b30bae5e11e033283368f1b85c9432b372abe36
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-04-29 12:29:25 +0200

    fixed locale

--- nixos/common.nix
+++ nixos/common.nix
@@ -1,6 +1,23 @@
 {pkgs, ...}:
 
 {
+  i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8" "en_DK.UTF-8/UTF-8"];
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LANG = "en_US.UTF-8";
+    LC_MESSAGES = "en_US.UTF-8";
+    LC_TIME = "en_DK.UTF-8";
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "en_DK.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_COLLATE = "de_DE.UTF-8";
+  };
+
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
   programs.firejail.enable = true;
