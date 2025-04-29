bebb08b1740f52723fde6463cb45e94ce137215a
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-04-29 12:30:22 +0200

    emoji font

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -5,10 +5,12 @@
       jetbrains-mono 
       nerd-fonts.jetbrains-mono
       noto-fonts-cjk-serif
+      noto-fonts-color-emoji
     ];
     fontconfig = {
       defaultFonts = {
         monospace = [ "JetBrainsMono" ];
+        emoji = [ "Noto Color Emoji" ];
       };
     };
   };
