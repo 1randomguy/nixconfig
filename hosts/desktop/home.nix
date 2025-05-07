ffd1d9939e402c1d9d6b149ff9a280a6248de057
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-05-07 15:18:04 +0200

    easyeffects eq

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -18,6 +18,190 @@
       ./gnome-config.nix
     ];
 
+  services.easyeffects = {
+    enable = true;
+    extraPresets = {
+      hd58x = {
+        output = {
+            "blocklist" = [];
+            "equalizer" = {
+                "input-gain" = -10.4;
+                "left" = {
+                    "band0" = {
+                        "frequency" = 25.0;
+                        "gain" = 6.0;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 0.71;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Low-Shelf";
+                    };
+                    "band1" = {
+                        "frequency" = 105.0;
+                        "gain" = 4.5;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 0.71;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Low-Shelf";
+                    };
+                    "band2" = {
+                        "frequency" = 160.0;
+                        "gain" = -3.0;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 0.55;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Bell";
+                    };
+                    "band3" = {
+                        "frequency" = 1350.0;
+                        "gain" = -2.2;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 1.5;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Bell";
+                    };
+                    "band4" = {
+                        "frequency" = 1900.0;
+                        "gain" = 4.5;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 0.71;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "High-Shelf";
+                    };
+                    "band5" = {
+                        "frequency" = 3250.0;
+                        "gain" = -3.8;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 2.1;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Bell";
+                    };
+                    "band6" = {
+                        "frequency" = 5400.0;
+                        "gain" = -7.0;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 3.5;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Bell";
+                    };
+                    "band7" = {
+                        "frequency" = 11000.0;
+                        "gain" = -4.0;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 0.71;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "High-Shelf";
+                    };
+                };
+                "mode" = "IIR";
+                "num-bands" = 8;
+                "output-gain" = 0.0;
+                "right" = {
+                    "band0" = {
+                        "frequency" = 25.0;
+                        "gain" = 6.0;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 0.71;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Low-Shelf";
+                    };
+                    "band1" = {
+                        "frequency" = 105.0;
+                        "gain" = 4.5;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 0.71;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Low-Shelf";
+                    };
+                    "band2" = {
+                        "frequency" = 160.0;
+                        "gain" = -3.0;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 0.55;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Bell";
+                    };
+                    "band3" = {
+                        "frequency" = 1350.0;
+                        "gain" = -2.2;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 1.5;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Bell";
+                    };
+                    "band4" = {
+                        "frequency" = 1900.0;
+                        "gain" = 4.5;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 0.71;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "High-Shelf";
+                    };
+                    "band5" = {
+                        "frequency" = 3250.0;
+                        "gain" = -3.8;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 2.1;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Bell";
+                    };
+                    "band6" = {
+                        "frequency" = 5400.0;
+                        "gain" = -7.0;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 3.5;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "Bell";
+                    };
+                    "band7" = {
+                        "frequency" = 11000.0;
+                        "gain" = -4.0;
+                        "mode" = "RLC (BT)";
+                        "mute" = false;
+                        "q" = 0.71;
+                        "slope" = "x1";
+                        "solo" = false;
+                        "type" = "High-Shelf";
+                    };
+                };
+                "split-channels" = false;
+            };
+            "plugins_order" = [
+                "equalizer"
+            ];
+        };
+      };
+    };
+  };
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "bene";
