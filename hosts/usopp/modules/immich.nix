75ba79cd50e12808d27c2af72065e463418a9133
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-05-19 17:18:10 +0200

    immich

--- hosts/usopp/modules/immich.nix
+++ hosts/usopp/modules/immich.nix
@@ -6,20 +6,4 @@
 
   users.users.immich.extraGroups = [ "video" "render" ];
 
-  services.nginx.enable = true;
-  services.nginx.virtualHosts.localhost = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://[::1]:${toString config.services.immich.port}";
-      proxyWebsockets = true;
-      recommendedProxySettings = true;
-      extraConfig = ''
-        client_max_body_size 50000M;
-        proxy_read_timeout   600s;
-        proxy_send_timeout   600s;
-        send_timeout         600s;
-      '';
-    };
-  };
 }
