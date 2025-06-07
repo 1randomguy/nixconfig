2a2b6e8e356f1007cf6f0a4d0839ad2e0c1a0945
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-06-07 22:59:05 +0200

    stuff

--- nixos/homelab/default.nix
+++ nixos/homelab/default.nix
@@ -6,7 +6,9 @@ in
 {
   imports = [
     ./immich
-    ./restic
+    ./adguard
+    #./smb
+    #./restic
   ];
 
   options.homelab = {
@@ -29,7 +31,7 @@ in
       default = "";
       type = lib.types.str;
       description = ''
-        Base domain name to be used to access the homelab services via Caddy reverse proxy
+        Base domain name to be used to access the homelab services via nginx reverse proxy
       '';
     };
   };
@@ -43,7 +45,37 @@ in
         uid = 994;
         isSystemUser = true;
         group = cfg.group;
+        extraGroups = [ "video" "render" "media" ];
+      };
+      networking.firewall.enable = true;
+      networking.firewall.allowPing = true;
+    };
+
+    age.secrets.porkbun = {
+      file = ../../../secrets/porkbun.age;
+    };
+    services.nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+    };
+    networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "bblomberg123@gmail.com";
+      certs."shimagumo.party" = {
+        dnsProvider = "porkbun";
+        environmentFile = config.age.secrets.porkbun.path;
       };
     };
+    #services.nginx.virtualHosts.localhost = {
+    #  locations."/" = {
+    #    return = "200 '<html><body>It works</body></html>'";
+    #    extraConfig = ''
+    #      default_type text/html;
+    #    '';
+    #  };
+    #};
   };
 }
--- nixos/homelab/immich/default.nix
+++ nixos/homelab/immich/default.nix
@@ -24,18 +24,35 @@ in
       services.immich = {
         enable = true;
         port = 2283;
+        user = hl.user;
         group = hl.group;
         # `null` will give access to all devices.
         # You may want to restrict this by using something like `[ "/dev/dri/renderD128" ]`
         accelerationDevices = null;
-        #environment = {
-        #  UPLOAD_LOCATION = "/data/immich_uploads";
-        #};
       };
       users.users.immich = {
         isSystemUser = true;
         group = "immich";
         extraGroups = [ "video" "render" "media" ];
+      };
+
+      services.nginx.virtualHosts."immich.shimagumo.party" = {
+        enableACME = true;
+        addSSL = true;
+        #forceSSL = true;
+        #sslCertificate = "/etc/ssl/selfsigned/selfsigned.crt";
+        #sslCertificateKey = "/etc/ssl/selfsigned/selfsigned.key";
+        locations."/" = {
+          proxyPass = "http://[::1]:${toString config.services.immich.port}";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+          extraConfig = ''
+            client_max_body_size 50000M;
+            proxy_read_timeout   600s;
+            proxy_send_timeout   600s;
+            send_timeout         600s;
+          '';
+        };
       };
   };
 }
