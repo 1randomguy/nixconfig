b414d6a08b7538b470adde939fb3ac81ab883e4d
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-06-24 23:31:42 +0200

    nfs more ports

--- nixos/homelab/nfs/default.nix
+++ nixos/homelab/nfs/default.nix
@@ -34,7 +34,8 @@ in
       '';
     };
     networking.firewall = {
-      allowedTCPPorts = [ 111 2049 ];
+      allowedTCPPorts = [ 111 2049 4000 4001 4002 20048 ];
+      allowedUDPPorts = [ 111 2049 4000 4001 4002 20048 ];
     };
   };
 }
