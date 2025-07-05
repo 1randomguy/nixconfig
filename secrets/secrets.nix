1759ec80aa8d83361d41b33176d3430a19adcd43
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-07-05 14:03:00 +0200

    agenix add desktop

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   usopp_server  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJavj6UCLWWqDZkG5monO+7WAdn/jFvUqblN9SzURip root@usopp";
+  desktop_bene_user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII32Ud21QSaA2cUChs6LvIP+oE3ZA3h+hKiteOZ6VZXE agenix_bene_desktop";
 in
 {
   "porkbun.age".publicKeys = [ usopp_server ];
-  "restic.age".publicKeys = [ usopp_server ];
+  "restic.age".publicKeys = [ usopp_server desktop_bene_user ];
 }
