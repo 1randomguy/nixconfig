373840d9be89adb074bd8053de57336a9e63f1fa
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-07-13 12:00:57 +0200

    fix immich app

--- nixos/homelab/immich/default.nix
+++ nixos/homelab/immich/default.nix
@@ -42,9 +42,10 @@ in
           proxy_set_header Host              $host;
           proxy_set_header X-Real-IP         $remote_addr;
           proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
 
-          proxy_redirect off;
+          # Something here broke the app
+          #proxy_set_header X-Forwarded-Proto $scheme;
+          #proxy_redirect off;
         '';
 
         locations."/" = {
