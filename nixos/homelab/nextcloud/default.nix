45181b43b3ff449e2d6677a93ef228d3e86fd196
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-08-20 17:20:20 +0200

    nextcloud: simplify

--- nixos/homelab/nextcloud/default.nix
+++ nixos/homelab/nextcloud/default.nix
@@ -18,29 +18,34 @@ in
     };
 
     # Set up backing up the database automatically. The path will be in
-    # `/var/backups/mysql/nextcloud.gz`.
-    services.mysqlBackup.databases = [ "nextcloud" ];
+    # `/var/backups/postgresql/nextcloud.gz`.
+    services.postgresqlBackup = {
+      enable = true;
+      databases = [ "nextcloud" ];
+      startAt = "*-*-* 02:00:00";  # Daily at 2 AM
+      location = "/var/backup/postgresql";
+    };
 
-    homelab.services.restic.backupDirs = [ "/var/lib/nextcloud/data" "/var/backups/mysql" ];
+    homelab.services.restic.backupDirs = [ "/var/lib/nextcloud" "/var/backups/postgresql" ];
 
     # Set up secrets. This is a sops-nix file checked in at the same folder as
-    # this file. #TODO: change to agenix
+    # this file. 
     age.secrets = {
       nextcloud_admin_password = {
-        file = ../../secrets/x;
+        file = ../../secrets/nextcloud_admin_password.age;
         owner = "nextcloud";
         group = "nextcloud";
       };
-      nextcloud_db_password = {
-        file = ../../secrets/x;
-        owner = "nextcloud";
-        group = "nextcloud";
-      };
-      nextcloud_secrets = {
-        file = ../../secrets/x;
-        owner = "nextcloud";
-        group = "nextcloud";
-      };
+      #nextcloud_db_password = {
+      #  file = ../../secrets/x;
+      #  owner = "nextcloud";
+      #  group = "nextcloud";
+      #};
+      #nextcloud_secrets = {
+      #  file = ../../secrets/x;
+      #  owner = "nextcloud";
+      #  group = "nextcloud";
+      #};
     };
 
     # Set up Nextcloud.
@@ -48,16 +53,28 @@ in
       enable = true;
       package = pkgs.nextcloud31;
       https = true;
+      enableBrokenCiphersForSSE = false;
+
       hostName = "cloud.${hl.baseDomain}";
-      secretFile = config.age.secrets.nextcloud_secrets.path;
+      #secretFile = config.age.secrets.nextcloud_secrets.path;
 
       phpOptions."opcache.interned_strings_buffer" = "13";
 
+      # Let NixOS install and configure Redis caching automatically.
+      configureRedis = true;
+
+      # Increase the maximum file upload size to avoid problems uploading videos.
+      maxUploadSize = "16G";
+      
+      # Let NixOS install and configure the database automatically.
+      database.createLocally = true;
+
       config = {
-        dbtype = "mysql";
-        dbname = "nextcloud";
-        dbhost = "localhost";
-        dbpassFile = config.age.secrets.nextcloud_db_password;
+        dbtype = "pgsql";
+        #dbname = "nextcloud";
+        #dbhost = "localhost";
+        #dbpassFile = config.age.secrets.nextcloud_db_password;
+
 
         adminuser = "admin";
         adminpassFile = config.age.secrets.nextcloud_admin_password;
@@ -68,27 +85,35 @@ in
         default_phone_region = "de";
         filelocking.enabled = true;
 
-        redis = {
-          host = config.services.redis.servers.nextcloud.bind;
-          port = config.services.redis.servers.nextcloud.port;
-          dbindex = 0;
-          timeout = 1.5;
-        };
+        #redis = {
+        #  host = config.services.redis.servers.nextcloud.bind;
+        #  port = config.services.redis.servers.nextcloud.port;
+        #  dbindex = 0;
+        #  timeout = 1.5;
+        #};
       };
 
       caching = {
         redis = true;
         memcached = true;
       };
+
+      autoUpdateApps.enable = true;
+      extraAppsEnable = true;
+      extraApps = with config.services.nextcloud.package.packages.apps; {
+        # List of apps we want to install and are already packaged in
+        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
+        inherit calendar contacts mail notes tasks;
+      };
     };
 
     # Set up Redis because the admin page was complaining about it.
     # https://discourse.nixos.org/t/nextlcoud-with-redis-distributed-cashing-and-file-locking/25321/3
-    services.redis.servers.nextcloud = {
-      enable = true;
-      bind = "::1";
-      port = 6379;
-    };
+    #services.redis.servers.nextcloud = {
+    #  enable = true;
+    #  bind = "::1";
+    #  port = 6379;
+    #};
 
     # Setup Nginx because we have multiple services on this server.
     services.nginx.virtualHosts."cloud.${hl.baseDomain}" = {
