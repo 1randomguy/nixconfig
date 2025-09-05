e4e366cbcd5b4750e5466b547a496b3952baea3e
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-09-05 18:01:35 +0200

    zola: extraConfig

--- nixos/homelab/zola/default.nix
+++ nixos/homelab/zola/default.nix
@@ -47,6 +47,12 @@ in
       enableACME = true;
       acmeRoot = null;
       forceSSL = true;
+      extraConfig = ''
+        # Set headers
+        proxy_set_header Host              $host;
+        proxy_set_header X-Real-IP         $remote_addr;
+        proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
+      '';
 
       locations."/" = {
         proxyPass = "http://127.0.0.1:${toString cfg.port}";
