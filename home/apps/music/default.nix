7ed846a2185fae25ccdf921ea38d9f8b86de7146
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-10-10 21:37:50 +0200

    music listening

--- home/apps/music/default.nix
+++ home/apps/music/default.nix
@@ -14,7 +14,8 @@ in {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       # music listening
-      #quodlibet
+      quodlibet
+      amberol
       spotify
     ];
     services.easyeffects = mkIf cfg.eq {
