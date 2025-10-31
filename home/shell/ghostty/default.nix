692e58fa8fd2c6897b65f9286d00eceb51adcb8e
Benedikt von Blomberg <git@bvb.anonaddy.com>
2025-10-31 11:43:58 +0100

    ghostty new keybinds

--- home/shell/ghostty/default.nix
+++ home/shell/ghostty/default.nix
@@ -19,10 +19,31 @@ in
         theme = "Catppuccin Mocha";
         font-size = 14;
         font-family = "JetBrainsMono";
-        # keybind = [
-        #   "ctrl+h=goto_split:left"
-        #   "ctrl+l=goto_split:right"
-        # ];
+        mouse-hide-while-typing = true;
+        scrollback-limit = 1000000;
+        window-save-state = "always";
+        
+        keybind = [
+          "ctrl+h=goto_split:left"
+          "ctrl+j=goto_split:bottom"
+          "ctrl+k=goto_split:top"
+          "ctrl+l=goto_split:right"
+
+          "ctrl+a>h=new_split:left"
+          "ctrl+a>j=new_split:down"
+          "ctrl+a>k=new_split:up"
+          "ctrl+a>l=new_split:right"
+          "ctrl+a>s=new_split:auto"
+          "ctrl+a>f=toggle_split_zoom"
+
+          "ctrl+a>c=new_tab"
+          "ctrl+a>ctrl+n=next_tab"
+          "ctrl+a>n=next_tab"
+          "ctrl+a>ctrl+p=previous_tab"
+          "ctrl+a>p=previous_tab"
+
+          "super+r=reload_config"
+        ];
       };
     };
   };
