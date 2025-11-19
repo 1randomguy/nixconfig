b47cf7996f67f6e9d06f45b9029d696f214f97d6
Benedikt von Blomberg <git@bvb.anonaddy.com>
2025-11-19 21:02:36 +0100

    niri calendar

--- nixos/workstation/niri/default.nix
+++ nixos/workstation/niri/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 let
   cfg = config.workstation.niri;
 in
@@ -10,5 +10,26 @@ in
   config = lib.mkIf cfg.enable {
     programs.niri.enable = true;
     services.displayManager.defaultSession = "niri";
+
+    ## for noctalia calender support:
+    services.gnome.evolution-data-server.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      (python3.withPackages (pyPkgs: with pyPkgs; [ pygobject3 ]))
+    ];
+
+    environment.sessionVariables = {
+      GI_TYPELIB_PATH = lib.makeSearchPath "lib/girepository-1.0" (
+        with pkgs;
+        [
+          evolution-data-server
+          libical
+          glib.out
+          libsoup_3
+          json-glib
+          gobject-introspection
+        ]
+      );
+    };
   };
 }
