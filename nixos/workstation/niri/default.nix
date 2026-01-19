2f8dc421a42ce05ee2c6caffe3704dda0b743546
Benedikt von Blomberg <git@bvb.anonaddy.com>
2026-01-19 21:02:26 +0100

    japanese for niri?!

--- nixos/workstation/niri/default.nix
+++ nixos/workstation/niri/default.nix
@@ -18,6 +18,26 @@ in
       (python3.withPackages (pyPkgs: with pyPkgs; [ pygobject3 ]))
     ];
 
+    # keyboard
+    services.xserver.xkb.layout = "us";
+    services.xserver.xkb.variant = "altgr-intl";
+    i18n.inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk      # GTK support
+        #fcitx5-configtool # GUI for configuration
+        qt6Packages.fcitx5-configtool
+      ];
+    };
+    environment.variables = {
+      NIXOS_OZONE_WL = "1"; # Hint electron apps to use wayland
+      #GTK_IM_MODULE = "fcitx";
+      #QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+    };
+
     environment.sessionVariables = {
       GI_TYPELIB_PATH = lib.makeSearchPath "lib/girepository-1.0" (
         with pkgs;
