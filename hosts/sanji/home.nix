1f5a4537efba793dd57e2e4c1b1e31e49c916147
Benedikt von Blomberg <git@bvb.anonaddy.com>
2026-01-27 23:12:41 +0100

    cleanup

--- hosts/sanji/home.nix
+++ hosts/sanji/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, inputs, ... }:
+{ ... }:
 
 {
   imports =
@@ -49,10 +49,6 @@
   home.username = "bene";
   home.homeDirectory = "/home/bene";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
@@ -75,20 +71,7 @@
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/bene/etc/profile.d/hm-session-vars.sh
-  #
+  # shell provided by Home Manager.
 
 
 
