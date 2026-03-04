69c6f5aabd35110f3a3d5ad6d5c00ba7c15aa1b6
Benedikt von Blomberg <git@bvb.anonaddy.com>
2026-03-04 16:19:44 +0100

    move lsp

--- modules/neovim/lua/plugins/init.lua
+++ modules/neovim/lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
   { import = "plugins.colorscheme" },
   { import = "plugins.snacks" },
+  { import = "plugins.lsp" },
   { import = "plugins.other" },
   { import = "plugins.ui" },
   { import = "plugins.rust" },
