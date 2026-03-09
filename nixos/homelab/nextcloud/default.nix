8b84a7b82f00c6a42c2d1cdbd7d305a9f59ef1c1
Benedikt von Blomberg <git@bvb.anonaddy.com>
2026-03-09 16:00:38 +0100

    nextcloud 32->33

--- nixos/homelab/nextcloud/default.nix
+++ nixos/homelab/nextcloud/default.nix
@@ -47,7 +47,7 @@ in
     # Set up Nextcloud.
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud32;
+      package = pkgs.nextcloud33;
       https = true;
 
       hostName = "nextcloud.${hl.baseDomain}";
