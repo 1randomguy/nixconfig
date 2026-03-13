a8cbe91667391b99b1d43857c184e7efcbaeb3b8
Benedikt von Blomberg <git@bvb.anonaddy.com>
2026-03-13 13:03:40 +0100

    cleanup

--- nixos/homelab/nextcloud/default.nix
+++ nixos/homelab/nextcloud/default.nix
@@ -37,11 +37,6 @@ in
         owner = "nextcloud";
         group = "nextcloud";
       };
-      nextcloud_secrets = {
-        file = ../../../secrets/nextcloud_secrets.age;
-        owner = "nextcloud";
-        group = "nextcloud";
-      };
     };
 
     # Set up Nextcloud.
@@ -70,7 +65,6 @@ in
         adminpassFile = config.age.secrets.nextcloud_admin_password.path;
       };
 
-      secretFile = config.age.secrets.nextcloud_secrets.path;
       settings = {
         maintenance_window_start = 2; # 02:00
         default_phone_region = "de";
@@ -79,35 +73,7 @@ in
         # Authelia OIDC
         allow_user_to_change_display_name = false;
         lost_password_link = "disabled";
-        #oidc_login_provider_url = "https://auth.${hl.baseDomain}";
-        #oidc_login_client_id = "Almkdw6nFOnuVxDW0SiMsO7RQetCVjJSobtnM.gDnSjvp~Dv2RsRKvPHxg~VOyE9lpY0Jwgz";
-        #oidc_login_auto_redirect = false;
-        #oidc_login_end_session_redirect = false;
-        #oidc_login_button_text = "Log in with Authelia";
-        #oidc_login_hide_password_form = false;
-        #oidc_login_use_id_token = false;
-        #oidc_login_attributes = {
-        #    id = "preferred_username";
-        #    name = "name";
-        #    mail = "email";
-        #    groups = "groups";
-        #    is_admin = "is_nextcloud_admin";
-        #};
-        #oidc_login_default_group = "oidc";
-        #oidc_login_use_external_storage = false;
-        #oidc_login_scope = "openid profile email groups nextcloud_userinfo";
-        #oidc_login_proxy_ldap = false;
-        #oidc_login_disable_registration = false;
-        #oidc_login_redir_fallback = false;
-        #oidc_login_tls_verify = true;
-        #oidc_create_groups = false;
-        #oidc_login_webdav_enabled = false;
-        #oidc_login_password_authentication = false;
-        #oidc_login_public_key_caching_time = 86400;
-        #oidc_login_min_time_between_jwks_requests = 10;
-        #oidc_login_well_known_caching_time = 86400;
-        #oidc_login_update_avatar = false;
-        #oidc_login_code_challenge_method = "S256";
+        ## for config of user_oidc look at nextcloud_secrets.age
       };
 
       caching = {
