706c5b4280244e6e54ec7f0b5936eae011e06439
Benedikt von Blomberg <git@bvb.anonaddy.com>
2026-03-20 11:14:24 +0100

    ja locale support

--- nixos/default.nix
+++ nixos/default.nix
@@ -5,7 +5,7 @@
     ./workstation
   ];
 
-  i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8" "en_DK.UTF-8/UTF-8"];
+  i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8" "en_DK.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8"];
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LANG = "en_US.UTF-8";
