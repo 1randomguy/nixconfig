d85a279c085d72539ac460380ebd52dc8af90284
Benedikt von Blomberg <git@bvb.anonaddy.com>
2026-03-27 14:17:20 +0100

    mmmm

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "locker flockig";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     #nixpkgs.url = "/home/bene/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
