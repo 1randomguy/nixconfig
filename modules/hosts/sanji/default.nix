2aa5490264e955785614219c9f612f7f7f924fbd
Benedikt von Blomberg <git@bvb.anonaddy.com>
2026-03-27 15:41:38 +0100

    fix

--- modules/hosts/sanji/default.nix
+++ modules/hosts/sanji/default.nix
@@ -1,15 +1,10 @@
-{ self, inputs, ... }:
+{ self, inputs, sharedNixpkgsConfig, ... }:
 {
   # Lenovo Thinkpad X9 15 "sanji"
   flake.nixosConfigurations.sanji = inputs.nixpkgs.lib.nixosSystem {
     modules = [
-      # {
-      #   imports = [ nixpkgs.nixosModules.readOnlyPkgs ];
-      #   nixpkgs.pkgs = pkgs;
-      # }
+      sharedNixpkgsConfig
       self.nixosModules.sanjiConfiguration
-      #self.nixosModules.neovim
-      # And then put `wrappers.nvim-test.enable = true;` in your configuration.nix
       inputs.agenix.nixosModules.default
       inputs.lanzaboote.nixosModules.lanzaboote
       inputs.home-manager.nixosModules.home-manager
