e8d13861324ff8e90241ec6b1492cdef62c505de
Benedikt von Blomberg <git@bvb.anonaddy.com>
2026-03-27 15:40:02 +0100

    move nvm

--- modules/wrapped/neovim/lua/plugins/language-support.lua
+++ modules/wrapped/neovim/lua/plugins/language-support.lua
@@ -4,17 +4,8 @@ return {
     lazy = false,
     auto_enable = true,
     after = function(plugin)
-      local ts_configs = require("nvim-treesitter.configs")
-      local ts_parsers = require("nvim-treesitter.parsers")
-
-      ts_configs.setup({
-        highlight = {
-          enable = true,
-          -- Disable on Nix because parsers are managed by Nix and already in rtp
-          -- But we keep it true here for general functionality
-        },
-      })
-
+      ---@param buf integer
+      ---@param language string
       local function treesitter_try_attach(buf, language)
         -- check if parser exists and load it
         if not vim.treesitter.language.add(language) then
@@ -22,10 +13,20 @@ return {
         end
         -- enables syntax highlighting and other treesitter features
         vim.treesitter.start(buf, language)
+
+        -- -- enables treesitter based folds
+        -- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        -- vim.wo.foldmethod = "expr"
+        -- -- ensure folds are open to begin with
+        -- vim.o.foldlevel = 99
+        --
+        -- -- enables treesitter based indentation
+        -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
         return true
       end
 
-      local installable_parsers = vim.tbl_keys(ts_parsers.get_parser_configs())
+      local installable_parsers = require("nvim-treesitter").get_available()
       vim.api.nvim_create_autocmd("FileType", {
         callback = function(args)
           local buf, filetype = args.buf, args.match
@@ -35,9 +36,9 @@ return {
           end
 
           if not treesitter_try_attach(buf, language) then
-            -- Only try to install if not on Nix
-            if not nixInfo.isNix and vim.tbl_contains(installable_parsers, language) then
-              require("nvim-treesitter.install").install(language):await(function()
+            if vim.tbl_contains(installable_parsers, language) then
+              -- not already installed, so try to install them via nvim-treesitter if possible
+              require("nvim-treesitter").install(language):await(function()
                 treesitter_try_attach(buf, language)
               end)
             end
@@ -63,17 +64,21 @@ return {
       -- vim.g.no_go_maps = true
     end,
     after = function(plugin)
-      -- nvim-treesitter-textobjects is typically configured via nvim-treesitter.configs
-      require("nvim-treesitter.configs").setup({
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            selection_modes = {
-              ['@parameter.outer'] = 'v',
-              ['@function.outer'] = 'V',
-            },
-            include_surrounding_whitespace = false,
+      require("nvim-treesitter-textobjects").setup {
+        select = {
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+          -- You can choose the select mode (default is charwise 'v')
+          --
+          -- Can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * method: eg 'v' or 'o'
+          -- and should return the mode ('v', 'V', or '<c-v>') or a table
+          -- mapping query_strings to modes.
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V',  -- linewise
+            -- ['@class.outer'] = '<c-v>', -- blockwise
           },
           -- If you set this to `true` (default is `false`) then any textobject is
           -- extended to include preceding or succeeding whitespace. Succeeding
@@ -86,7 +91,7 @@ return {
           -- and should return true of false
           include_surrounding_whitespace = false,
         },
-      })
+      }
 
       -- keymaps
       -- You can use the capture groups defined in `textobjects.scm`
