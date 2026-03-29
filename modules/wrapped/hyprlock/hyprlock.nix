4c8bb3a9704028e33b695a50673d0f8c8379ab74
Benedikt von Blomberg <bblomberg123@gmail.com>
2026-03-29 10:49:46 +0200

    smart hyprlock cmd

--- modules/wrapped/hyprlock/hyprlock.nix
+++ modules/wrapped/hyprlock/hyprlock.nix
@@ -4,8 +4,8 @@
   ...
 }:
 {
-  perSystem = { pkgs, system, ... }: {
-    packages.ashell = inputs.wrapper-modules.lib.wrapPackage ({ config, lib, ... }: {
+  perSystem = { pkgs, self', system, ... }: {
+    packages.hyprlock = inputs.wrapper-modules.lib.wrapPackage ({ config, lib, ... }: {
       inherit pkgs; 
       package = pkgs.hyprlock;
       binName = "hyprlock";
@@ -13,5 +13,19 @@
         "--config" = "${./hyprlock.conf}";
       };
     });
+    packages.hyprlockSmart = pkgs.writeShellApplication {
+      name = "lock";
+      text = ''
+        if ! ${pkgs.procps}/bin/pgrep -x hyprlock > /dev/null; then
+          ${self'.packages.hyprlock}/bin/hyprlock&
+          # ${self'.packages.hyprlock}/bin/hyprlock > /dev/null 2>&1 &
+          # # Disown the process so the shell completely forgets about it
+          # disown
+          # This is the "Magic Buffer" that kills the flicker
+          ${pkgs.coreutils}/bin/sleep 1
+        fi
+      '';
+    };
+
   };
 }
