79f9a192a1cfcbc8ac32d576b08418fb22b42e55
Benedikt von Blomberg <bblomberg123@gmail.com>
2026-03-30 15:17:11 +0200

    cleanup apps

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -1,15 +1,6 @@
-{lib, config, pkgs, ...}:
-with lib;
-let
-  cfg = config.apps;
-in
+{...}:
 {
   imports = [
     ./music
   ];
-  options.apps = {
-    enable = mkEnableOption "Install graphical apps on this system";
-  };
-  config = mkIf cfg.enable {
-  };
 }
