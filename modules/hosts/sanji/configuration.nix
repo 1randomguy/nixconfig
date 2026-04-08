fd8bf923919da82186028a3ed9b757eeb2c1b41c
Benedikt <bblomberg123@gmail.com>
2026-04-08 22:20:57 +0200

    Update configuration.nix

--- modules/hosts/sanji/configuration.nix
+++ modules/hosts/sanji/configuration.nix
@@ -24,6 +24,7 @@
         self.nixosModules.master-thesis
         self.nixosModules.uni-vpn
         self.nixosModules.games
+        self.nixosModules.local-llm
       ];
 
       games.bottles.enable = true;
