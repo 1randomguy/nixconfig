156c90e9d569abb3bd4886174e0184b1cc3b2133
Benedikt von Blomberg <bblomberg123@gmail.com>
2026-05-23 16:16:54 +0200

    immich behind authelia

--- modules/nixos/homelab/services/authelia.nix
+++ modules/nixos/homelab/services/authelia.nix
@@ -82,7 +82,10 @@
               {
                 domain = [ "nextcloud.${hl.baseDomain}" ];
                 policy = "bypass"; # other option: "two_factor"
-                networks = [ "192.168.178.0/24" "100.0.0.0/8" ];
+                networks = [
+                  "192.168.178.0/24"
+                  "100.0.0.0/8"
+                ];
               }
               {
                 domain = [ "nextcloud.${hl.baseDomain}" ];
@@ -91,7 +94,10 @@
               {
                 domain = [ "music.${hl.baseDomain}" ];
                 policy = "bypass";
-                networks = [ "192.168.178.0/24" "100.0.0.0/8" ];
+                networks = [
+                  "192.168.178.0/24"
+                  "100.0.0.0/8"
+                ];
               }
               {
                 domain = [ "music.${hl.baseDomain}" ];
@@ -100,12 +106,15 @@
               {
                 domain = [ "immich.${hl.baseDomain}" ];
                 policy = "bypass"; # other option: "two_factor"
-                #networks = [ "192.168.178.0/24" ];
+                networks = [
+                  "192.168.178.0/24"
+                  "100.0.0.0/8"
+                ];
               }
-              #{
-              #  domain = ["immich.${hl.baseDomain}"];
-              #  policy = "one_factor"; # other option: "two_factor"
-              #}
+              {
+                domain = [ "immich.${hl.baseDomain}" ];
+                policy = "one_factor"; # other option: "two_factor"
+              }
             ];
           };
 
