c6c39c72fee0a063150496f44c14f0ac6d41f281
Benedikt von Blomberg <bblomberg123@gmail.com>
2026-05-23 15:26:53 +0200

    nextcloud fix number one

--- modules/nixos/homelab/services/authelia.nix
+++ modules/nixos/homelab/services/authelia.nix
@@ -79,11 +79,15 @@
                 domain = [ "${hl.baseDomain}" ];
                 policy = "one_factor"; # other option: "two_factor"
               }
-              # {
-              #   domain = [ "nextcloud.${hl.baseDomain}" ];
-              #   policy = "one_factor"; # other option: "two_factor"
-              #   networks = [ "192.168.178.0/24" "100.0.0.0/8" ];
-              # }
+              {
+                domain = [ "nextcloud.${hl.baseDomain}" ];
+                policy = "bypass"; # other option: "two_factor"
+                networks = [ "192.168.178.0/24" "100.0.0.0/8" ];
+              }
+              {
+                domain = [ "nextcloud.${hl.baseDomain}" ];
+                policy = "one_factor"; # other option: "two_factor"
+              }
               {
                 domain = [ "music.${hl.baseDomain}" ];
                 policy = "bypass";
