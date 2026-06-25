1c320c396c48d98d0e40d67b8f45a9a800ac237f
Benedikt von Blomberg <bblomberg123@gmail.com>
2026-06-25 16:42:38 +0200

    zsh dehomify

---
+++ modules/wrapped/zsh/zsh.nix
@@ -0,0 +1,113 @@
+{
+  inputs,
+  self,
+  ...
+}:
+{
+  flake.zshWrapper =
+    {
+      config,
+      wlib,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [ wlib.wrapperModules.zsh ];
+
+      options.p10k = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Whether to enable p10k prompt";
+      };
+      options.historyKeymaps = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Whether to enable history keymaps";
+      };
+
+      config = {
+        skipGlobalRC = true;
+        env.EDITOR.set = "nvim";
+        zshrc.content = lib.mkMerge [
+          ''
+            # --- Base Configuration ---
+            # Replaces history.size = 10000;
+            HISTSIZE=10000
+            SAVEHIST=10000
+
+            # Replaces enableCompletion, autosuggestion, syntaxHighlighting
+            source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+            source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+            autoload -Uz compinit && compinit
+
+            # --- Oh My Zsh Setup ---
+            # To simulate HM's oh-my-zsh module without HM, source the main file 
+            # and explicitly load the requested plugins from the store package
+            export ZSH="${pkgs.oh-my-zsh}/share/oh-my-zsh"
+            plugins=(git sudo colored-man-pages extract tmux)
+            for plugin in $plugins; do
+              if [ -f "$ZSH/plugins/$plugin/$plugin.plugin.zsh" ]; then
+                source "$ZSH/plugins/$plugin/$plugin.plugin.zsh"
+              fi
+            done
+
+            source "$ZSH/oh-my-zsh.sh"
+
+            source ${pkgs.zsh-you-should-use}/share/zsh/plugins/you-should-use/you-should-use.plugin.zsh
+          ''
+
+          (lib.mkIf config.historyKeymaps ''
+            # these don't work in wsl e.g.
+            bindkey "''${key[Up]}" up-line-or-search
+            bindkey "''${key[Down]}" down-line-or-search
+          '')
+
+          # --- Powerlevel10k Conditional Block ---
+          (lib.mkIf config.p10k ''
+            # Instant prompt implementation
+            if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+              source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+            fi
+
+            # Source the standalone theme script
+            source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+
+            # Look for your custom p10k.zsh file relative to where this flake module lives
+            [[ ! -f ${./config/p10k.zsh} ]] || source ${./config/p10k.zsh}
+
+            # Replaces home.packages = [ any-nix-shell ] via inline invocation
+            ${pkgs.any-nix-shell}/bin/any-nix-shell zsh --info-right | source /dev/stdin
+          '')
+
+          # --- Post-Initialization Block (mkAfter equivalent) ---
+          ''
+            eval "$(direnv hook zsh)"
+            autoload -Uz edit-command-line
+            zle -N edit-command-line
+            bindkey "^x^e" edit-command-line
+          ''
+        ];
+      };
+    };
+
+  perSystem =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      packages.zsh = inputs.wrapper-modules.wrappers.zsh.wrap {
+        inherit pkgs;
+        imports = [ self.zshWrapper ];
+      };
+      packages.zsh-mini = inputs.wrapper-modules.wrappers.zsh.wrap {
+        inherit pkgs;
+        imports = [
+          self.zshWrapper
+          { p10k = false; }
+        ];
+      };
+    };
+}
