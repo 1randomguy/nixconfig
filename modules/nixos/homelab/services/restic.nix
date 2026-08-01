916298c8420ce5fa694c2930a581a31372384c65
Benedikt von Blomberg <bblomberg123@gmail.com>
2026-08-01 17:33:35 +0200

    restic: module refactor

--- modules/nixos/homelab/services/restic.nix
+++ modules/nixos/homelab/services/restic.nix
@@ -4,67 +4,71 @@
     let
       cfg = config.homelab.services.restic;
       hl = config.homelab;
+      pgBackupLocation = "/var/backup/postgresql";
+      pgBackupEnable = ((config.services.postgresqlBackup.databases or [ ]) != [ ]);
+      backupDirs = cfg.backupDirs ++ lib.optionals pgBackupEnable [ pgBackupLocation ];
     in
     {
-      config = lib.mkIf (cfg.backupDirs != [ ]) {
-        age.secrets.restic = {
-          file = ../../../../secrets/restic.age;
-        };
-        age.secrets.backblazeb2 = {
-          file = ../../../../secrets/backblazeb2.age;
-        };
-        systemd.tmpfiles.rules = lib.lists.optionals cfg.local.enable [
-          "d ${cfg.local.targetDir} 0770 ${hl.user} ${hl.group} - -"
-        ];
-        services.postgresqlBackup = {
-          enable = true;
-          startAt = "*-*-* 02:00:00"; # Daily at 2 AM
-          location = "/var/backup/postgresql";
-        };
+      config = lib.mkIf (backupDirs != [ ]) (lib.mkMerge [
+        {
+          age.secrets.restic.file = ../../../../secrets/restic.age;
 
-        services.restic = {
-          backups =
-            lib.attrsets.optionalAttrs cfg.local.enable {
-              appdata-local = {
-                timerConfig = {
-                  OnCalendar = "*-*-* 03:00:00";  # 1 hour after the postgresql backup
-                  Persistent = true;
-                };
-                repository = cfg.local.targetDir;
-                initialize = true;
-                passwordFile = config.age.secrets.restic.path;
-                pruneOpts = [
-                  "--keep-daily 7"
-                  "--keep-weekly 5"
-                  "--keep-monthly 12"
-                ];
-                exclude = [
-                ];
-                paths = cfg.backupDirs ++ [ "var/lib/postgresql" ];
+          services.postgresqlBackup = lib.mkIf pgBackupEnable {
+            enable = true;
+            startAt = "*-*-* 02:00:00"; # Daily at 2 AM
+            location = pgBackupLocation;
+          };
+        }
+        (lib.mkIf cfg.local.enable {
+          systemd.tmpfiles.rules = [
+            "d ${cfg.local.targetDir} 0770 ${hl.user} ${hl.group} - -"
+          ];
+
+          services.restic.backups = {
+            appdata-local = {
+              timerConfig = {
+                OnCalendar = "*-*-* 03:00:00"; # 1 hour after the postgresql backup
+                Persistent = true;
               };
-            }
-            // lib.attrsets.optionalAttrs cfg.s3.enable {
-              appdata-s3 = {
-                timerConfig = {
-                  #OnCalendar = "Sun *-*-* 06:00:00";
-                  OnCalendar = "*-*-* 03:00:00"; # 1 hour after the postgresql backup
-                  Persistent = true;
-                };
-                environmentFile = config.age.secrets.backblazeb2.path;
-                repository = "s3:s3.eu-central-003.backblazeb2.com/3YLELy";
-                initialize = true;
-                passwordFile = config.age.secrets.restic.path;
-                pruneOpts = [
-                  "--keep-daily 1"
-                  "--keep-weekly 3"
-                  "--keep-monthly 5"
-                ];
-                exclude = [
-                ];
-                paths = cfg.backupDirs ++ [ "var/lib/postgresql" ];
-              };
+              repository = cfg.local.targetDir;
+              initialize = true;
+              passwordFile = config.age.secrets.restic.path;
+              pruneOpts = [
+                "--keep-daily 7"
+                "--keep-weekly 5"
+                "--keep-monthly 12"
+              ];
+              exclude = [
+              ];
+              paths = backupDirs;
             };
-        };
-      };
+          };
+        })
+        (lib.mkIf cfg.s3.enable {
+          age.secrets.backblazeb2.file = ../../../../secrets/backblazeb2.age;
+
+          services.restic.backups = {
+            appdata-s3 = {
+              timerConfig = {
+                #OnCalendar = "Sun *-*-* 06:00:00";
+                OnCalendar = "*-*-* 03:00:00"; # 1 hour after the postgresql backup
+                Persistent = true;
+              };
+              environmentFile = config.age.secrets.backblazeb2.path;
+              repository = "s3:s3.eu-central-003.backblazeb2.com/3YLELy";
+              initialize = true;
+              passwordFile = config.age.secrets.restic.path;
+              pruneOpts = [
+                "--keep-daily 1"
+                "--keep-weekly 3"
+                "--keep-monthly 5"
+              ];
+              exclude = [
+              ];
+              paths = backupDirs;
+            };
+          };
+        })
+      ]);
     };
 }
